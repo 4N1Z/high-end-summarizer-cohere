11b8b2a784ef4689dfbf8246e9345af248539173
4N1Z <20am014@sctce.ac.in>
2023-10-14 16:27:01 +0530

    pdf functionality added

---
+++ README.md
@@ -0,0 +1,63 @@
+# High-End Summariser using cohere endpoints
+
+Welcome to the High-End Summariser project repository!
+
+High-End Summariser is an advanced tool designed to condense crucial information from large documents, accommodating up to 50,000 characters. This project leverages Cohere's foundational language model and offers a range of customizable settings for efficient summarization.
+
+## Table of Contents
+- [Project Overview](#project-overview)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Examples](#examples)
+- [Frequently Asked Questions](#frequently-asked-questions)
+- [Support](#support)
+- [Acknowledgements](#acknowledgements)
+
+## Project Overview
+
+The High-End Summariser project focuses on providing an effective solution for summarizing lengthy documents. It utilizes Cohere's powerful language model to process large texts, extracting key insights and creating concise summaries. This project is aimed at developers and individuals seeking advanced text summarization capabilities.
+
+## Getting Started
+
+To start using the High-End Summariser project, follow these steps:
+
+1. **Clone the Repository:** Begin by cloning this repository to your local machine using the following command:
+   ```
+   git clone https://github.com/Cohere-io/summarizer-high-end.git
+   ```
+2. **Set Up Dependencies:** Ensure you have the necessary dependencies installed. You might need Python, Cohere API credentials, or any specific libraries mentioned in the project documentation.
+   
+3. **Explore the Codebase:** Dive into the project code to understand its structure, functions, and customizable settings.
+
+## Usage
+
+The High-End Summariser project offers a user-friendly interface for summarizing text. Developers can seamlessly integrate the summarization functionality into their applications or workflows. To use the project effectively, understand the input parameters and output indicators, as described in the project documentation.
+
+## Examples
+
+Explore the provided examples to learn how to integrate the High-End Summariser into your applications:
+
+- [Sample Code](https://github.com/Cohere-io/summarizer-high-end/blob/main/example.py): This code demonstrates the usage of the Cohere Generate endpoint for various generative tasks, including text summarization, long-form content generation, entity extraction, and copywriting.
+
+## Frequently Asked Questions
+
+**Q: What is the maximum document length that can be processed?**
+A: The system can handle documents with a maximum length of 50,000 characters.
+
+**Q: What if I need to process longer documents?**
+A: While the beta endpoint is currently limited to 50,000 characters, we are actively working to expand this limit. Stay updated on our progress through our [Summarise product page](https://txt.cohere.com/summarize-beta/).
+
+**Q: Encountered a problem with the endpoint?**
+A: If you face any issues, please don't hesitate to [contact our support team](mailto:support@cohere.com).
+
+## Support
+
+If you require assistance, you can reach out to us through the following channels:
+
+- [Submit a Support Ticket](mailto:support@cohere.com)
+- [Live Chat](https://txt.cohere.com/chat)
+- [Email Support](mailto:support@cohere.com)
+
+## Acknowledgements
+
+We appreciate your use of Cohere's High-End Summariser. We hope this tool proves invaluable in quickly and accurately condensing essential information from documents. If you have feature requests or discover any bugs, please [submit an issue](https://github.com/Cohere-io/summarizer-high-end/issues) on our GitHub repository.
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import cohere
 import streamlit as st
 from nlpSummarizer import formattingForSummarizer
+import PyPDF2
+import re
+
 co = cohere.Client(st.secrets["COHERE_API_KEY"]) 
 
 # with open('content1.txt', 'r', encoding='utf-8') as file:
 #     text = file.read()
+st.header('SSW-Summarizer')
+st.write('High End Summarizer Using Cohere Endpoints - Summarize any documents')
 
-st.title('High End Summarizer Using Cohere Endpoints')
 
+def split_text_into_sentences(text):
+    # Split text into sentences using regular expressions
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s', text)
+    return sentences
 
 def summarzerMain(text) :
   responses = []
@@ -24,12 +32,12 @@ def summarzerMain(text) :
         with st.spinner('Generating response...'):
           for text in [text1, text2]:
             response = co.summarize( 
-              text=text,
-              length='long',
-              format='paragraph',
-              model='summarize-xlarge',
-              additional_command= summarizer_prompt,
-              temperature=0.3,
+                  text=text,
+                  length='long',
+                  format='bullets',
+                  model='summarize-xlarge',
+                  additional_command= summarizer_prompt,
+                  temperature=0.3,
             ) 
             print(response)
             responses.append(response.summary)
@@ -38,21 +46,22 @@ def summarzerMain(text) :
             print("PHASE 1 ONGOINGGGG")
 
           # Add a header for the summary
-          st.markdown("<h2 style='color: green;'>Summary:</h2>", unsafe_allow_html=True)
+          st.markdown("<h3 style='color: green;'>Summary:</h3>", unsafe_allow_html=True)
           # After summarizing add these responses into co.chat and make it upto 300 words
           print("PHASE 1 Completed______________________")
           print("PHASE 2 ONGOING")
 
           chatResponses = ' '.join(responses)
-          prompt_template = "Enlarge the given content into 5 paragraph response of `MORE THAN 500 WORDS` each. The content is : " + ' ' + chatResponses
+          prompt_template = "Enlarge the given content into n number of paragraph response of `MORE THAN 500 WORDS` each. The content is : " + ' ' + chatResponses + "Give Response in a VERY EFFICIENT FORMAT, Preferabbly in BULLETS"
           response = co.generate(
-            model='command-nightly',
-            prompt= prompt_template,
-            max_tokens= 400,
-            temperature=0.3,
-            k=0,
-            stop_sequences=[],
-            return_likelihoods='NONE'
+                model='command',
+                prompt= prompt_template,
+                num_generations = 3,
+                max_tokens= 400,
+                temperature=0.3,
+                k=0,
+                stop_sequences=[],
+                return_likelihoods='NONE'
             )
           
           
@@ -72,22 +81,61 @@ def summarzerMain(text) :
 
 def main ():
    
-  uploaded_file = st.file_uploader("Upload a file", type=["txt"]) 
+  uploaded_file = st.file_uploader("Upload a file", type=["txt", "pdf"]) 
+
+  if 'read_more_clicked' not in st.session_state:
+    st.session_state.read_more_clicked = False
+
   if uploaded_file is not None:
-
-    text = uploaded_file.read().decode('utf-8')
-    st.markdown("<h2 style='color: red;'>Uploaded Content:</h2>", unsafe_allow_html=True)
-    st.write('Word Count:', len(text.split()))
-    st.write(text)
-    text = formattingForSummarizer(text)
-
-    # Add a header for the original text
-    # st.markdown("<h2 style='color: blue;'>Original Text:</h2>", unsafe_allow_html=True)
-    # st.write(text)
-    # split the text into 2 equal halves
-
+    file_extension = uploaded_file.name.split(".")[-1]
     
-    summarzerMain(text)
+    if file_extension.lower() == "pdf":
+        # Read text from the PDF file
+        pdf_reader = PyPDF2.PdfReader(uploaded_file)
+        pdf_text = ""
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num] 
+            pdf_text += page.extract_text()
+
+        # Process the PDF text (you might need additional logic for PDF-specific formatting)
+        st.markdown("<h4 style='color: #9D9D9D;'>Uploaded Content (PDF):</h4>", unsafe_allow_html=True)
+        # Split text into sentences
+        sentences = split_text_into_sentences(pdf_text)
+        st.write('Word Count:', len(pdf_text.split()))
+
+        # Display limited content with "Read More" functionality
+        num_displayed_sentences = 2
+        if len(sentences) > num_displayed_sentences:
+            displayed_content = " ".join(sentences[:num_displayed_sentences])
+            st.write(displayed_content)
+            # if st.button("Read More"):
+            #     st.write(" ".join(sentences))
+            #     st.session_state.read_more_clicked = False
+        else:
+            st.write(pdf_text)
+        processed_text = formattingForSummarizer(pdf_text)
+
+    elif file_extension.lower() == "txt":
+        # Read text from the TXT file
+        text = uploaded_file.read().decode('utf-8')
+
+        # Process the text
+        st.markdown("<h4 style='color: #9D9D9D;'>Uploaded Content (PDF):</h4>", unsafe_allow_html=True)
+        st.write('Word Count:', len(text.split()))
+        # st.write(text)
+        sentences = split_text_into_sentences(text)
+        num_displayed_sentences = 2
+        if len(sentences) > num_displayed_sentences:
+            displayed_content = " ".join(sentences[:num_displayed_sentences])
+            st.write(displayed_content)
+
+        else:
+            st.write(text)
+        processed_text = formattingForSummarizer(text)
+    else:
+        st.write("Unsupported file format: As of now we only have the option to read a TXT or PDF file.")
+
+    summarzerMain(processed_text)
 
 
 if __name__ == "__main__" :
